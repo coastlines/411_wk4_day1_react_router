ab95a489cd807170346071a7693187d8a069e953
efaulkenberry@gmail.com <efaulkenberry@gmail.com>
2020-08-25 20:16:50 -0500

    Router.js, Added router to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Navigation from './components/Navigation'
-import './App.css'
 // Write imports for Router & BrowserRouter here //
+import BrowserRouter from 'react-router-dom'
+import Router from './Router'
+import './App.css'
 
 function App() {
   return (
--- src/Router.js
+++ src/Router.js
@@ -10,8 +10,20 @@ Remember to export the component at the end
 
 import React from 'react'
 import { Switch, Route } from 'react-router'
+
 // Write component imports here //
 
+import Home from './Components/Home'
+import About from './Components/About'
 
 
 // Start Router function here //
+
+export default Router = () => {
+    return (
+        <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/about' component={About} />
+        </Switch>
+    )
+}
