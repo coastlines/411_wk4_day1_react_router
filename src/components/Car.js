a1cd3db4f2b249840e78b835951eb5e9315b0116
efaulkenberry@gmail.com <efaulkenberry@gmail.com>
2020-08-27 19:28:20 -0500

    car container, paper, chip + find car by id

--- src/components/Car.js
+++ src/components/Car.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import cars from '../cars.json'
 // import material ui components here //
+import  { Container, Paper, Chip } from '@material-ui/core'
 // Container, Paper, Chip //
 
 const Car = (props) => {
+let id = props.match.params.id;
+
+const car = cars.find(car => car.id === Number(id));
+
+console.log(car)
+
     return (
-        <h1>A specific car</h1>
+        <Container maxWidth="sm" className="car-container">
+            <Paper elevation={3} className="car-paper">
+                <h1>{car.Name}</h1>
+                {
+                    Object.keys(car).map((key, index) => {
+                        return <Chip className="car-chip" label={`${key}: ${car[key]}`}></Chip>
+                    })
+                }
+            </Paper>
+        </Container> 
     )
 }
 
